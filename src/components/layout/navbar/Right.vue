3034d70bae5486547117b09295fcb4c16ddbbec7
João Vítor Dias <joaodias@noctus.org>
2017-05-07 21:32:58 -0300

    Right navbar

--- src/components/layout/navbar/Right.vue
+++ src/components/layout/navbar/Right.vue
@@ -5,6 +5,29 @@ export default {
 
 <template>
   <ul class="nav navbar-nav navbar-right">
-    <li><a href="#">Link</a></li>
+    <li>
+      <a href="#">
+        <i class="fa fa-th"></i>
+        <span class="visible-xs-inline">Aplicativos</span>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i class="fa fa-bell"></i>
+        <span class="visible-xs-inline">Notificações</span>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i class="fa fa-cog"></i>
+        <span class="visible-xs-inline">Configurações</span>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i class="fa fa-user-circle"></i>
+        <span class="visible-xs-inline">Perfil</span>
+      </a>
+    </li>
   </ul>
 </template>
