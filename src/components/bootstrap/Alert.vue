0107e03dc4b17c7a26e6a6ef66418dcfbe666442
João Vítor Dias <joaodias@noctus.org>
2017-05-15 15:24:57 -0300

    close alert

--- src/components/bootstrap/Alert.vue
+++ src/components/bootstrap/Alert.vue
@@ -3,13 +3,23 @@ export default {
   props: {
     msg: { type: String },
     type: { type: String, default: 'danger' }
+  },
+  data () {
+    return {
+      isOpen: true
+    }
+  },
+  methods: {
+    close () {
+      this.isOpen = false
+    }
   }
 }
 </script>
 
 <template>
-  <div class="alert alert-dismissible text-uppercase" :class="'alert-'+type">
-    <button type="button" class="close" data-dismiss="alert">&times;</button>
+  <div v-show="isOpen" class="alert alert-dismissible text-uppercase" :class="'alert-'+type">
+    <button type="button" class="close" @click="close">&times;</button>
     <slot>{{ msg }}</slot>
   </div>
 </template>
