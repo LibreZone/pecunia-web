50bf8b83c73793a09c6d7478adf8d48a6a8dd57f
João Vítor Dias <joaodias@noctus.org>
2017-05-15 15:22:03 -0300

    redirect to login if is not logged in

--- src/router/beforeEach.js
+++ src/router/beforeEach.js
@@ -1,5 +1,13 @@
+import store from '@/store'
+
+const isLogged = store.state.Auth.isLogged
+
 const beforeEach = (to, from, next) => {
-  next()
+  if (!isLogged && to.name !== 'auth.login') {
+    return next({ name: 'auth.login' })
+  }
+
+  return next()
 }
 
 export default beforeEach
