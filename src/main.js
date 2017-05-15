d0353fb2609aaa5ddbe4d6b386fd2e6573e2b5cd
João Vítor Dias <joaodias@noctus.org>
2017-05-14 22:45:20 -0300

    vuex

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App'
 import router from './router'
 import NProgress from 'vue-nprogress'
 import firebase from './plugins/firebase'
+import store from './store'
 
 Vue.use(firebase)
 Vue.use(NProgress)
@@ -13,6 +14,7 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
   nprogress,
   template: '<App/>',
